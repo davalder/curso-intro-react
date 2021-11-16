3c79e448941cbf36524a29b0abeba70d62a2108a
davalder <davalder@hotmail.com>
2021-11-15 20:36:06 -0500

    clase 5: Componentes de TODO Machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+import { CreateTodoButton } from "./CreateTodoButton";
+// import './App.css';
 
-function App(props) {
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cortar tomate', completed: false },
+  { text: 'Cortar zanahoria', completed: false },
+]
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> and save to reload.
-          {props.saludo}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.children}
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+      
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <App saludo="Hola">
-    Buenaaaaass
-  </App>,
+  <App />,
   document.getElementById('root')
 );
