56c4eea5da42ae788785d0513f8f830b2b34cc9b
davalder <davalder@hotmail.com>
2021-11-15 21:22:58 -0500

    clase 6: CSS en React

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { CreateTodoButton } from "./CreateTodoButton";
 
 const todos = [
   { text: 'Cortar cebolla', completed: false },
-  { text: 'Cortar tomate', completed: false },
+  { text: 'Cortar tomate', completed: true },
   { text: 'Cortar zanahoria', completed: false },
 ]
 
@@ -16,17 +16,20 @@ function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-
       <TodoSearch />
-      
+
       <TodoList>
         {todos.map(todo => (
-          <TodoItem key={todo.text} text={todo.text} />
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            />
         ))}
       </TodoList>
 
       <CreateTodoButton />
-      
+
     </React.Fragment>
   );
 }
